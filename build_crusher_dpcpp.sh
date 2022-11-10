c8ed90e0801e3d735771a884c318c682cd1ba683
jholmen <holmenjk@ornl.gov>
2022-11-10 13:41:34 -0500

    Added DPC++ build script.

---
+++ build_crusher_dpcpp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+module load cmake PrgEnv-cray
+module -t list
+
+rm -rf build
+mkdir build
+
+cd build
+
+cmake -DCMAKE_C_COMPILER=/gpfs/alpine/gen243/proj-shared/holmenjk/crusher/llvm/build_5.2.0/bin/clang -DCMAKE_CXX_COMPILER=/gpfs/alpine/gen243/proj-shared/holmenjk/crusher/llvm/build_5.2.0/bin/clang++ -DENABLE_SYCL=ON -DENABLE_EXAMPLES=ON -DCMAKE_CXX_FLAGS="-std=c++17 -O3 -sycl-std=2020 -fsycl -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx90a -I/opt/cray/pe/mpich/8.1.17/ofi/cray/10.0/include" ../src/
+
+cmake --build . -j 32
+
+cd StandAlone
+
+cp sus ../../tmp/sus
+cp compare_uda ../../tmp/compare_uda
