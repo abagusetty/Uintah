b00976c7a5f9d9cfcd8571c83e66348f044e51e7
Abhishek Bagusetty <abagusetty@anl.gov>
2023-02-28 16:29:50 +0000

    [SYCL] add initial cmake support for kokkos/mdspan

--- src/configure_options.cmake
+++ src/configure_options.cmake
@@ -135,7 +135,25 @@ if( ENABLE_SYCL )
   set(CMAKE_CXX_STANDARD 17)
 
   # set SYCL flags for AoT compilation for ATS, PVC devices
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsycl -sycl-std=2020 -fsycl-device-code-split=per_kernel -lze_loader")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsycl -sycl-std=2020 -fsycl-device-code-split=per_kernel")
+
+  # Downlaod and install Kokkos/mdspan
+  find_package(mdspan QUIET)
+  if (NOT mdspan_FOUND)
+      message(STATUS "No installed mdspan found, fetching from Github")
+      include(FetchContent)
+      FetchContent_Declare(
+          mdspan
+          GIT_REPOSITORY  https://github.com/kokkos/mdspan.git
+          GIT_TAG         stable
+          GIT_PROGRESS    TRUE
+      )
+      FetchContent_MakeAvailable(mdspan)
+  endif()
+
+  if(NOT TARGET std::mdspan)
+      find_package(mdspan REQUIRED)
+  endif()
 
   if( ENABLE_OCCL )
     find_dependency( OCCL )
